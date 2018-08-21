ff4aaee0081a446038ead90da14bf98f9ad18242
992903713 <992903713@qq.com>
2018-08-21 19:12:24 +0800

    leetcode: morris solution for 99

--- leetcode problems/99. Recover Binary Search Tree.cpp
+++ leetcode problems/99. Recover Binary Search Tree.cpp
@@ -7,7 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
+class Solution1 {
 public:
     TreeNode *pre;
     TreeNode *first;
@@ -32,5 +32,58 @@ public:
             pre = root;
         }
         inorder(root->right);
+    }
+};
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution2 {
+public:
+    void recoverTree(TreeNode* root) {
+        /*
+         * morrisÖÐÐò±éÀú
+         */
+        if(!root) return;
+
+        TreeNode *first = NULL,*second = NULL,*parent = NULL;
+
+        TreeNode *cur1 = root;
+        TreeNode *cur2 = NULL;
+        while(cur1){
+            cur2 = cur1->left;
+            if(cur2){
+                while(cur2->right && cur2->right != cur1){
+                    cur2 = cur2->right;
+                }
+                if(!cur2->right){
+                    cur2->right = cur1;
+                    cur1 = cur1->left;
+                    continue;
+                }else{
+                    cur2->right = NULL;
+                    if(parent->val > cur1->val){
+                        if(!first) first = parent;
+                        second = cur1;
+                    }
+                    parent = cur1;
+                }
+            }
+
+            if(parent && parent->val > cur1->val){
+                if(!first)  first = parent;
+                second = cur1;
+            }
+            parent = cur1;
+            cur1 = cur1->right;
+        }
+
+
+        if(first && second) swap(first->val,second->val);
     }
 };
