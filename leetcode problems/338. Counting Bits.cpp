53e748181d9cb636219281806defcd8ad089b462
992903713 <992903713@qq.com>
2018-09-17 09:08:22 +0800

    leetcode: Flatten Nested List Iterator

--- leetcode problems/338. Counting Bits.cpp
+++ leetcode problems/338. Counting Bits.cpp
@@ -15,6 +15,21 @@ public:
 class Solution2 {
 public:
     vector<int> countBits(int num) {
+        vector<int> dp;
+        dp.push_back(0);
+        for(int i=1; i<=num; ++i){
+            if(i % 2 == 0){
+                dp.push_back(dp[i/2]);
+            }else{
+                dp.push_back(dp[i/2] + 1);
+            }
+        }
+        return dp;
+    }
+};
+class Solution3 {
+public:
+    vector<int> countBits(int num) {
         vector<int> res;
         for(int i=0; i<=num; ++i){
             int sum = 0;
