75424e5e78f0363982dfcb411564d5c6e25ab7c1
钟奇龙 <zhongqilong@zhongqilongdeMacBook-Pro.local>
2018-11-22 14:47:11 +0800

    18-11-22 394. Decode String

---
+++ leetcode problems/394. Decode String.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        int i=0;
+        return decode(s,i);
+    }
+    string decode(string s,int &i){
+        string res = "";
+        int n = s.size();
+        while(i < n && s[i] != ']'){
+            if(!(s[i] >= '0' && s[i] <= '9')){
+                res += s[i++];
+            }else{
+                int cnt = 0;
+                while(i < n && s[i] >= '0' && s[i] <= '9'){
+                    cnt = cnt*10 + s[i++]-'0';
+                }
+                i++;
+                string next = decode(s,i);
+                i++;
+                while(cnt){
+                    res += next;
+                    cnt--;
+                }
+            }
+        }
+        
+        return res;
+    }
+};
